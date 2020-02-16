37232806aa0631b13c577ca902a31f2ba0e169ff
luwei <867285912@qq.com>
2020-02-16 15:04:57 +0800

    分布项目整合，mongoDB

---
+++ study_files/springboot_dubbo/dubbo_provider/src/main/java/com/luwei/dubbo_provider/service/impl/MongoDbServiceImpl.java
@@ -0,0 +1,23 @@
+package com.luwei.dubbo_provider.service.impl;
+
+import com.luwei.entity.User;
+import com.luwei.service.MongoDbService;
+
+import java.util.List;
+
+/**
+ * @projectName： GitHub
+ * @packageName: com.luwei.dubbo_provider.service.impl
+ * @auther: luwei
+ * @description:
+ * @date: 2020/2/16 15:03
+ * @alert: This document is private to luwei
+ * @version: 1.8.00_66
+ */
+public class MongoDbServiceImpl implements MongoDbService {
+
+    @Override
+    public List<User> save(User user) {
+        return null;
+    }
+}
