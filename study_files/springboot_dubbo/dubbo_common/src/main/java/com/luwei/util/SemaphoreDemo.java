82a7060e564b5b39a3aa80db5821600db00fe9ab
luwei867285912@163.com <luwei13320367983>
2020-03-12 14:39:38 +0800

    时间->LocalDate

---
+++ study_files/springboot_dubbo/dubbo_common/src/main/java/com/luwei/util/SemaphoreDemo.java
@@ -0,0 +1,42 @@
+package com.luwei.util;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+
+    // 排队总人数（请求总数）
+    public static int clientTotal = 10;
+
+    // 可同时受理业务的窗口数量（同时并发执行的线程数）
+    public static int threadTotal = 3;
+
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    resolve(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+    private static void resolve(int i) throws InterruptedException {
+        System.out.println("正在执行任务:"+i);
+        Thread.sleep(2000);
+    }
+}
