0890f6f80adbb0c65d3e8be1f2ede6638014e927
luwei867285912@163.com <luwei13320367983>
2020-03-19 09:13:54 +0800

    本地缓存

--- study_files/springboot_dubbo/dubbo_common/src/main/java/com/luwei/util/CacheDemo.java
+++ study_files/springboot_dubbo/dubbo_common/src/main/java/com/luwei/util/CacheDemo.java
@@ -8,15 +8,20 @@ import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-
+/**
+ * <p>@description : 缓存操作 </p>
+ * <p>@author : Wei.Lu</p>
+ * <p>@date : 2020/3/19 8:26 </p>
+ */
 public class CacheDemo {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         Cache<String, List<String>> build = CacheBuilder.newBuilder()
-                .expireAfterAccess(600, TimeUnit.SECONDS)
+                .expireAfterWrite(2, TimeUnit.SECONDS) // 设置过期时间，TimeUnit.SECONDS是指的多少秒过期
                 .build();
 
         for (int i = 0; i < 10; i++) {
+            Thread.sleep(1000);
             List<String> getValue = build.getIfPresent("getValue");
             System.out.println("第一次拿取值:"+getValue);
             if (CollectionUtils.isEmpty(getValue)) {
