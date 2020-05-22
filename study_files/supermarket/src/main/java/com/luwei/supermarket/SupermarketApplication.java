85743ed7e3af408d72b014e17ed3d637ba07e912
luwei867285912@163.com <luwei13320367983>
2020-05-22 13:16:00 +0800

    结构调整

--- study_files/supermarket/src/main/java/com/luwei/supermarket/SupermarketApplication.java
+++ study_files/supermarket/src/main/java/com/luwei/supermarket/SupermarketApplication.java
@@ -1,5 +1,6 @@
 package com.luwei.supermarket;
 
+import com.luwei.supermarket.base.SuperClientApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan("com.luwei.supermarket.mapper")
 @EnableTransactionManagement(proxyTargetClass = true) // 开启数据库事务管理
-public class SupermarketApplication {
+public class SupermarketApplication extends SuperClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(SupermarketApplication.class, args);
     }
